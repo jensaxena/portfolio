1f71568da44685edaceeedd24e5a7eed729f4bac
Jen Saxena <jen@jensaxena.com>
2017-12-10 16:31:54 -0500

    Sidebar now updates when scrolling manually, not just on click

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,32 +1,75 @@
 // NAMESPACE
 const folio = {};
 
-// NAVIGATION FUNCTIONS
+// RETURN TO TOP ON REFRESH
+folio.goBack = function() {
+  // TO DO: ANIMATE OR FIND BETTER SOLUTION TO AVOID WHITE FLASH
+  $(window).on('beforeunload', function() {
+    $(window).scrollTop(0);
+  });
+}
+
+// FADE IN/OUT DROPDOWN NAV
 folio.fade = function() {
-  $('#menu-collapse').on('click', function() {
-    $('#dropdown').fadeToggle(900);
-  })
+  $('#dropdown').fadeToggle(900);
 };
 
-folio.scroller = function() {
+// DO FADE ON MENU ICON CLICK
+folio.dropdown = function() {
+  $('#menu-collapse').on('click', function() {
+    folio.fade();
+  })
+}
+
+// CHANGE SIDEBAR LOCATION INDICATOR
+folio.scrolling = function() {
+
+  // GET PAGE LOCATION
+  $(window).scroll(function() {
+    folio.location = $(this).scrollTop();
+
+    // GET SECTION AT LOCATION
+    $('section').each(function() {
+      folio.target = $(this).offset().top;
+      folio.targetID = $(this).attr('id');
+
+      // UPDATE CURRENT LOCATION
+      if (folio.location >= folio.target) {
+        $('.nav__sidebar--link').removeClass('nav__sidebar--active');
+        $(`#${folio.targetID}--active`).addClass('nav__sidebar--active');
+      }
+    })
+  })
+}
+
+// GO DIRECTLY TO SECTION
+folio.scrollTo = function() {
   $('a[href^="#"]').on('click', function(e) {
     e.preventDefault();
     folio.section = $(this.getAttribute('href'));
 
-    $('html').stop().animate({scrollTop: folio.section.offset().top}, 1000);
-    $('#dropdown').fadeOut(900);
+    $('html, body').stop().animate({scrollTop:folio.section.offset().top}, 1000);
 
-    $('.nav__sidebar--link').removeClass('nav__sidebar--active');
-    $(`#${folio.section[0].id}--active`).addClass('nav__sidebar--active');
+    if ($('#dropdown').is(':visible')) {
+      folio.fade();
+    }
   })
 };
 
 // TO DO: EMAIL FORM ON SUBMIT FUNCTIONS
+// folio.form = function() {
+//   $('form').on('submit', function(e) {
+//     e.preventDefault();
+//     // window.location.href = 'mailto:jen@jensaxena.com';
+//   })
+// };
 
 // FIRE AT WILL
 folio.init = function() {
-  folio.fade();
-  folio.scroller();
+  folio.goBack();
+  folio.dropdown();
+  folio.scrollTo();
+  folio.scrolling();
 };
 
 // DOCUMENT READY
